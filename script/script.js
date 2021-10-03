432697e6ab578a6ebb7c1c91b8bae69d67dd6cce
I Ketut Gede Mahendra Saputra <48759474+MahendraSaputraa@users.noreply.github.com>
2021-10-03 18:08:36 +0800

    p

--- script/script.js
+++
@@ -1,7 +0,0 @@
-var togle = document.querySelector('.menu-toggle input');
-var nav = document.querySelector('nav ul');
-
-
-togle.addEventListener('click', function() {
-    nav.classList.toggle('slide');
-})
